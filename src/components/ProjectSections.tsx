6ce912b9d3861e9a399252ec60db4443dcfcc257
kanchan-git-projects <156053973+kanchan-git-projects@users.noreply.github.com>
2024-03-14 10:25:36 +0530

    Update ProjectSections.tsx

--- src/components/ProjectSections.tsx
+++ src/components/ProjectSections.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "../components/ProjectCard";
 const ProjectSections = () => {
   return (
     <div className="px-5 py-2 overfloy-y-scroll" style={{ height: "65vh" }}>
-      <nav>Navigation Bar</nav>
+      <nav> All Projects</nav>
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
           <div className="col-span-3 p-2 bg-gray-300 rounded-lg sm:col-span-6 lg:col-span-4">
